e3a4124e5424447860a9b3f77aacf82f19a4e8b7
IsmailHosen01 <ismailhossen12340@gmail.com>
2025-10-23 18:51:09 +0600

    books card fixed

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Book = ({ singleBook }) => {
-    const { bookName, author, image, category, rating } = singleBook;
+    const { bookName, author, image, category, rating, tags } = singleBook;
     console.log(singleBook)
     return (
         <div className="card bg-base-100 shadow-sm p-5">
@@ -11,8 +11,13 @@ const Book = ({ singleBook }) => {
                     src={image}
                     alt="Shoes" />
             </figure>
-            <div className="space-y-1.5">
-                <h2 className="card-title playfair-display-font font-bold mt-4">{bookName}</h2>
+            <div className="space-y-2">
+                <div className='flex gap-10 text-[#23BE0A] font-medium mt-4'>
+                    {
+                        tags.map((tag,index) => <button key={index} className='bg-[#23be0a0d] px-4 py-2 rounded-full'>{tag}</button>)
+                    }
+                </div>
+                <h2 className="card-title playfair-display-font font-bold">{bookName}</h2>
                 <p className='text-[#131313cc] font-medium border-b-1 border-dashed border-gray-300 pb-3'>By: {author}</p>
                 <div className="flex justify-between items-center text-[#131313cc] font-medium">
                         <p>{category}</p>
