b9742e4d8c00b53a3256d314156c9c2a320c5306
IsmailHosen01 <ismailhossen12340@gmail.com>
2025-10-25 21:57:47 +0600

    sort functionality implement

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -9,15 +9,45 @@ const ReadList = () => {
     const data = useLoaderData();
 
     const [readList, setReadList] = useState([]);
+    const [sort, setSort] = useState("");
 
-    useEffect(()=> {
+    useEffect(() => {
         const storedBookData = getStoredBook()
         const convertedStoredBookData = storedBookData.map(id => parseInt(id));
         const myReadList = data.filter(book => convertedStoredBookData.includes(book.bookId));
         setReadList(myReadList)
-    },[data])
+    }, [data])
+
+    const handleSort = (type) => {
+        setSort(type);
+
+        if (type === "rating") {
+            const sortedByRating = [...readList].sort((a, b) => a.rating - b.rating);
+            setReadList(sortedByRating)
+        }
+        if (type === "pages") {
+            const sortedByPages = [...readList].sort((a, b) => a.totalPages - b.totalPages);
+            setReadList(sortedByPages)
+        }
+        if (type === "year") {
+            const sortedByYear = [...readList].sort((a, b) => a.yearOfPublishing - b.yearOfPublishing);
+            setReadList(sortedByYear)
+        }
+    }
     return (
         <div className='my-4'>
+
+            <div className='text-center'>
+                <details className="dropdown">
+                    <summary className="btn m-1 bg-[#23BE0A] text-white">Sort By  {sort === 'pages' && `: ${sort}`} {sort === 'rating' && `: ${sort}`} {sort === 'year' && `: ${sort}`}</summary>
+                    <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a onClick={() => handleSort("rating")}>Rating</a></li>
+                        <li><a onClick={() => handleSort("pages")}>Number of Pages</a></li>
+                        <li><a onClick={() => handleSort("year")}>Publisher year</a></li>
+                    </ul>
+                </details>
+            </div>
+
             <Tabs>
                 <TabList>
                     <Tab>Read Books</Tab>
